30b8929a4a889bce3e0b5b90d8eaad0abb40a6d1
Rahul Kumar <rahulkumarjha58978@gmail.com>
2025-01-31 00:38:48 +0530

    created frontend for job-portal also created partial navbar

---
+++ frontend/src/components/Avatar.jsx
@@ -0,0 +1,25 @@
+import {useState} from 'react'
+export default function Avatar() {
+    const [photohai,setphotohai]=useState(false);
+    return (
+      <>
+  
+        <div className="flex -space-x-2 overflow-hidden">
+            {!photohai && <>
+            
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Unknown_person.jpg/925px-Unknown_person.jpg"  className="inline-block size-10 rounded-full ring-2 ring-white" alt="userpic" />
+            </>}
+            {photohai &&
+                <img
+                alt=""
+                src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                className="inline-block size-10 rounded-full ring-2 ring-white"
+              />
+                
+            }
+          
+        </div>
+      </>
+    )
+  }
+  
