a5f74bd5fec56285e5d810a7ccf31fdfea2ca475
Rahul Kumar <rahulkumarjha58978@gmail.com>
2025-02-07 18:48:03 +0530

    made login and register page also added event listener so that we can store data both in register and login page in useState

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,52 @@
-import React from 'react'
-
+import React,{useState} from 'react'
+import { Link } from 'react-router-dom'
+import Navbar from '../components/Navbar.jsx'
 function Login() {
+  const [input,setinput]=useState({
+    email:"",
+    password:"",
+    role:""
+  })
+
+  const changeEventHandler=(e)=>{
+    setinput({...input,[e.target.name]:e.target.value})
+  }
+
+  const submitHandler=(e)=>{
+    e.preventDefault();
+    console.log(input)
+  }
   return (
-    <div>Login</div>
+    <>
+      <div>
+      <Navbar />
+      <div className='w-full h-full flex items-center justify-center'>
+        <form onSubmit={submitHandler} className='flex flex-col gap-2 p-3 w-3/5'>
+          <h1 className='font-semibold text-center text-black text-2xl underline'>Login</h1>
+          <label>Email</label>
+          <input className='p-1 rounded-sm bg-slate-100' 
+            name="email"
+            onChange={changeEventHandler}
+          type="text" placeholder="rahul@gmail.com" />
+          <label>Password</label>
+          <input className='p-1 rounded-sm bg-slate-100'
+            name="password"
+            onChange={changeEventHandler}
+          type="password" placeholder="password" />
+          <div className='flex gap-2 flex-wrap'>
+            Applicant:<input className='p-1 rounded-sm bg-slate-100' type="radio" name="role" 
+            onChange={changeEventHandler}
+            value="applicant" />
+            Recruiter:<input className='p-1 rounded-sm bg-slate-100' type="radio" name="role" 
+            onChange={changeEventHandler}
+            value="recruiter" />
+          </div>
+          <button className='bg-black text-white p-1 rounded-md mt-1'>Login</button>
+          <span>Don't have an Account ? <Link to='/register' className='text-slate-600 hover:text-blue-900 hover:text-lg hover:font-semibold'>Signup</Link></span>
+        </form>
+      </div>
+    </div>
+    </>
   )
 }
 
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,8 +1,68 @@
-import React from 'react'
-
+import React,{useState} from 'react'
+import { Link } from 'react-router-dom'
+import Navbar from '../components/Navbar'
 function Register() {
+  const [input,setinput]=useState({
+    name:"",
+    email:"",
+    phonenumber:"",
+    password:"",
+    role:"",
+  });
+
+  const changeEventHandler=(e)=>{
+    setinput({...input,[e.target.name]:e.target.value});
+  }
+
+  const changeFilehandler=(e)=>{
+    setinput({...input,file:e.target.files?.[0]});
+  }
+
+  const onSubmitHandler=(e)=>{
+    e.preventDefault();
+    console.log(input);
+  }
   return (
-    <div>Register</div>
+    <div>
+      <Navbar />
+      <div className='w-full h-full flex items-center justify-center'>
+        <form onSubmit={onSubmitHandler} className='flex flex-col gap-2 p-3 w-3/5'>
+          <h1 className='font-semibold text-center text-black text-2xl underline'>Register</h1>
+          <label>Full Name</label>
+          <input className='p-1 rounded-sm bg-slate-100'
+            name="name"
+            onChange={changeEventHandler}
+           type="text" placeholder="rahul" />
+          <label>Email</label>
+          <input className='p-1 rounded-sm bg-slate-100' 
+             name="email"
+             onChange={changeEventHandler}
+          type="text" placeholder="rahul@gmail.com" />
+          <label>Phone Number</label>
+          <input className='p-1 rounded-sm bg-slate-100'
+             name="phonenumber"
+             onChange={changeEventHandler}
+          type="text" placeholder="989987898" />
+          <label>Password</label>
+          <input className='p-1 rounded-sm bg-slate-100'
+             name="password"
+             onChange={changeEventHandler}
+          type="password" placeholder="password" />
+          <div className='flex gap-2 flex-wrap'>
+            Applicant:<input className='p-1 rounded-sm bg-slate-100' type="radio" name="role"
+               onChange={changeEventHandler}
+            value="applicant" />
+            Recruiter:<input className='p-1 rounded-sm bg-slate-100' type="radio" name="role" 
+             onChange={changeEventHandler}
+            value="recruiter" />
+          </div>
+          <label>Profile</label>
+          <input className='p-1 rounded-sm bg-slate-100' type="file" onChange={changeFilehandler} />
+          <button className='bg-black text-white p-1 rounded-md mt-1'> Signup</button>
+          <span>Already have an account ? <Link to='/login' className='text-slate-600 hover:text-blue-900 hover:text-lg hover:font-semibold'>Login</Link></span>
+        </form>
+      </div>
+    </div>
   )
 }
 
