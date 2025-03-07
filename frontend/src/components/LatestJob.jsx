9b90a2020b35d4f6f215cc332f6aade7993134f0
Rahul Kumar <rahulkumarjha58978@gmail.com>
2025-03-07 18:01:31 +0530

    pata ni ye latest job section q ni gy changes me

--- frontend/src/components/LatestJob.jsx
+++ frontend/src/components/LatestJob.jsx
@@ -7,7 +7,7 @@ function LatestJob() {
         <h1 className='text-3xl font-semibold text-black mb-4'><span className='text-red-700'>Latest & Top </span>Job Openings</h1>
         <div className='grid grid-cols-1 gap-4 sm:grid-cols-2 sm:gap-4 md:grid-cols-3 md:gap-x-4 md:gap-y-4'>
             {/* {job cards} */}
-            {randomno.slice(0,6).map(item=><div className='w-full bg-blue-300 p-2 rounded-md '>
+            {randomno.slice(0,6).map(item=><div className='w-full bg-blue-200 p-2 rounded-md '>
                 <h1>Company Name</h1>
                 <h2>India</h2>
                 <h3>Job Title</h3>
