56c0a2bf83695c13984186f8bfae29c5374c90f4
Rahul Kumar <rahulkumarjha58978@gmail.com>
2025-06-21 10:11:20 +0530

    add jobSlice.js file so that we could get to know about the state of jobs

---
+++ frontend/src/redux/jobSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const jobSlice=createSlice({
+    name:"job",
+    intitalState:{
+        allJobs:[]
+    },
+    reducers:{
+        setallJobs:(state,action)=>{
+            state.allJobs=action.payload;
+        }
+    }
+})
+export const {setallJobs}=jobSlice.actions;
+export default jobSlice.reducer;
