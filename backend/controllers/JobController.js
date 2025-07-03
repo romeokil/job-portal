6162e59700719da8b1c18a49105e2bf2b6ee253b
Rahul Kumar <rahulkumarjha58978@gmail.com>
2025-07-03 11:55:53 +0530

    we are trying to populate the applications under singlejob so that we can track which user applied for which job , trying this only if not works we will find out some other way

--- backend/controllers/JobController.js
+++ backend/controllers/JobController.js
@@ -84,7 +84,7 @@ export const getAllJobs=async(req,res)=>{
 export const getJobById=async(req,res)=>{
     try{
             const jobId=req.params.id;
-            const job=await Job.findById(jobId);
+            const job=await Job.findById(jobId).populate('applications');
             if(!job){
                 return res.status(404).json({
                     message:"Sorry Job not found!",
