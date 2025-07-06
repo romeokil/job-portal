8c7324b9f3edaea7e29de75e7e9ba723549e0480
Rahul Kumar <rahulkumarjha58978@gmail.com>
2025-07-06 12:03:45 +0530

    this is the company register route works fine

--- frontend/src/admin/Admincreatecompanies.jsx
+++ frontend/src/admin/Admincreatecompanies.jsx
@@ -1,14 +1,37 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Navbar from '../components/Navbar'
 import { useNavigate } from 'react-router-dom'
 
 function Admincreatecompanies() {
+    const [loading,setloading]=useState(false);
+    const [companyname,setcompanyname]=useState('');
     const Navigate=useNavigate();
     const backtoadmin=()=>{
         Navigate('/admin/companies')
     }
-    const nextpage=()=>{
-        Navigate('/admin/companies/desc')
+    const registercompany= async ()=>{
+        try {
+            setloading(true);
+            const response=await fetch('http://localhost:8000/api/company/register',{
+                method:'POST',
+                headers:{
+                    'Content-Type':'application/JSON'
+                },
+                body:JSON.stringify({companyname}),
+                credentials:'include'
+            })
+            if(response.ok){
+                const data=await response.json();
+                const registeredcompany=data.newCompany;
+                console.log(registeredcompany);
+                Navigate(`/admin/companies/desc/${registeredcompany._id}`);
+            }
+        } catch (error) {
+            console.log("Error while registering company");
+        }
+        finally{
+            setloading(false);
+        }
     }
   return (
     <>
@@ -20,11 +43,11 @@ function Admincreatecompanies() {
             </div>
             <div className='mt-6'>
                 <p className='text-black font-medium mb-1'>Company Name</p>
-                <input className='p-1 w-full' type="text"  placeholder="JobHunt,Microsoft"/>
+                <input onChange={(e)=>setcompanyname(e.target.value)}className='p-1 w-full' type="text"  placeholder="JobHunt,Microsoft"/>
             </div>
             <div className='my-5 flex gap-3'>
                 <button className='text-black p-2 font-medium' onClick={backtoadmin}>Cancel</button>
-                <button className='text-white bg-black p-2 font-medium rounded-lg' onClick={nextpage}>Continue</button>
+                <button className='text-white bg-black p-2 font-medium rounded-lg' onClick={registercompany}>Continue</button>
             </div>
 
         </div>
