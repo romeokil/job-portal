d0bea7cf2bf529b8604d089b9db866682f78296c
Rahul Kumar <rahulkumarjha58978@gmail.com>
2025-07-13 20:03:46 +0530

    this is a new hook to fetch alladminjobs in the hooks folder

---
+++ frontend/src/hooks/usegetallAdminJobs.jsx
@@ -0,0 +1,30 @@
+import React,{useEffect} from 'react'
+import { useDispatch } from 'react-redux'
+import { setalladminJobs} from '../redux/jobSlice.js';
+
+const usegetallAdminJobs= () => {
+    const dispatch=useDispatch();
+    useEffect(()=>{
+        const fetchallAdminJobs= async ()=>{
+        try{
+            const response=await fetch(`http://localhost:8000/api/jobs/getadminjob`,{
+            method:'GET',
+            credentials:'include'
+            })
+
+            if(response.ok){
+            let newresponse=await response.json();
+            console.log("All Admin jobs retrieved!!");
+            console.log(newresponse);
+            dispatch(setalladminJobs(newresponse.jobs));
+            }
+        }
+        catch(error){
+            console.log("Error while fetching all Admin jobs!!1")
+        }
+        }
+        fetchallAdminJobs();
+    },[])
+}
+
+export default usegetallAdminJobs
