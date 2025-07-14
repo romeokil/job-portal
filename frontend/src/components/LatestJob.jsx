265764995736f3e9d9d368cd42c6e50bb2a264c5
Rahul Kumar <rahulkumarjha58978@gmail.com>
2025-07-14 18:01:37 +0530

    esme jo blue wala ui tha usko apne andr wala ui se change kr diye hai abhi ke liye but ye ui bhi perfect ni hai deformed ho ja rha hai toh make sure mt last me isko thik kro

--- frontend/src/components/LatestJob.jsx
+++ frontend/src/components/LatestJob.jsx
@@ -1,28 +1,47 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
+import Jobcard from './Jobcard';
 
 function LatestJob() {
-  const Jobs=useSelector((state)=>state.job.allJobs);
-  console.log("Latest job component ke ander->",Jobs)
+  const Jobs = useSelector((state) => state.job.allJobs);
+  console.log("Latest job component ke ander->", Jobs)
   // let randomno=[1,2,3,4,5,6,7,8];
   return (
     <div className='w-4/5 mx-auto'>
-        <h1 className='text-3xl font-semibold text-black mb-4'><span className='text-red-700'>Latest & Top </span>Job Openings</h1>
-        <div className='grid grid-cols-1 gap-4 sm:grid-cols-2 sm:gap-4 md:grid-cols-3 md:gap-x-4 md:gap-y-4'>
-            {/* {job cards} */}
-
-            {Jobs.length<=0?<span>No Jobs Available.</span>:Jobs.slice(0,6).map(job=><div className='w-full bg-blue-200 p-2 rounded-md '>
-                <h1>{job.company.name}</h1>
-                <h2>{job.location}</h2>
-                <h3>{job.title}</h3>
-                <p>{job.description}</p>
-                <div className='flex justify-between'>
-                    <p>{job.position} Positions</p>
-                    <p>{job.jobType}</p>
-                    <p>{job.salary} LPA</p>
+      <h1 className='text-3xl font-semibold text-black mb-4'><span className='text-red-700'>Latest & Top </span>Job Openings</h1>
+      <div className='grid grid-cols-1 gap-4 sm:grid-cols-2 sm:gap-4 md:grid-cols-3 md:gap-x-4 md:gap-y-4'>
+        {/* {job cards} */}
+        {
+          Jobs.length <= 0 ? <span>No Latest Job available.
+          </span> : <>
+            {
+              Jobs.map(job => {
+                return <div>
+                  <div className='w-full rounded-md border-2 border-slate-300 shadow-lg shadow-slate-500/50 p-2'>
+                    <div className='flex gap-1'>
+                      <div className='w-20 border border-slate-200'>
+                        <img src={job?.company?.logo}></img>
+                      </div>
+                      <div className='flex gap-1 flex-col'>
+                        <div className='text-md font-bold text-black'>{job.company.name}</div>
+                        <div className='text-sm font-semibold text-black'>{job.location}</div>
+                      </div>
+                    </div>
+                    <h2 className='text-lg font-bold'>{job.title}</h2>
+                    <p className='text-sm text-slate-500'>{job.description}</p>
+                    <div className='flex gap-3 mt-1 mb-1'>
+                      <p className='text-sm font-medium text-black border border-slate-200'>{job.position} positions</p>
+                      <p className='text-sm font-medium text-red-600  border border-slate-200'>{job.jobType}</p>
+                      <p className='text-sm font-medium text-black border border-slate-200'>{job.salary} LPA</p>
+                    </div>
+                  </div>
                 </div>
-                </div>)} 
-        </div>
+              })
+            }
+          </>
+        }
+
+      </div>
     </div>
   )
 }
