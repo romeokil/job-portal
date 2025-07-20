96ab10298d6645b3f8cbb3027a0b57b5cbcd8532
Rahul Kumar <rahulkumarjha58978@gmail.com>
2025-07-20 13:01:24 +0530

    admincreatecompanies table dark mode enabled

--- frontend/src/admin/Admincreatecompanies.jsx
+++ frontend/src/admin/Admincreatecompanies.jsx
@@ -5,6 +5,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { setsinglecompany} from '../redux/companyslice.js'
 
 function Admincreatecompanies() {
+    const isDark=useSelector((state)=>state.theme.isDark);
     const dispatch=useDispatch();
     const [loading,setloading]=useState(false);
     const [companyname,setcompanyname]=useState('');
@@ -40,17 +41,17 @@ function Admincreatecompanies() {
   return (
     <>
         <Navbar/>
-        <div className='w-4/5 mx-auto m-5'>
+        <div className={`${isDark?'bg-slate-900':'bg-white'} w-full h-screen p-5 mx-auto`}>
             <div className='mt-6 mb-3'>
-                <h2 className='text-black font-semibold text-2xl'>Your Company Name</h2>
+                <h2 className={`${isDark?'text-white':'text-black'} font-semibold text-2xl`}>Your Company Name</h2>
                 <p className='text-slate-600 font-medium text-xs'>What would you like to give your company name? you can change this later.</p>
             </div>
             <div className='mt-6'>
-                <p className='text-black font-medium mb-1'>Company Name</p>
-                <input onChange={(e)=>setcompanyname(e.target.value)}className='p-1 w-full' type="text"  placeholder="JobHunt,Microsoft"/>
+                <p className={`${isDark?'text-white':'text-black'} text-black font-medium mb-1`}>Company Name</p>
+                <input className='w-full p-1 rounded-lg' onChange={(e)=>setcompanyname(e.target.value)}  type="text"  placeholder="JobHunt,Microsoft"/>
             </div>
             <div className='my-5 flex gap-3'>
-                <button className='text-black p-2 font-medium' onClick={backtoadmin}>Cancel</button>
+                <button className={`${isDark?'text-white':'text-black'} p-2 font-medium`} onClick={backtoadmin}>Cancel</button>
                 <button className='text-white bg-black p-2 font-medium rounded-lg' onClick={registercompany}>Continue</button>
             </div>
 
